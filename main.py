9665a0daa4b850026248467fa05ae7c96588a304
GHhuang1057 <huang1057@outlook.com>
2025-06-12 21:52:30 +0800

    1111

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui.main_window import Ui_MainWindow
+
+class MainWindow(Ui_MainWindow):
+    def __init__(self, window):
+        super().__init__()
+        self.setupUi(window)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = MainWindow(window)
+    window.show()
+    sys.exit(app.exec_())
